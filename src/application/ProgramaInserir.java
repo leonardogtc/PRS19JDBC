3c9cf012ec6baba6dd855c0a50f1e0b3f608b6f8
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-05-25 10:31:13 -0300

    Inserindo dados na tabela de vendedores

---
+++ src/application/ProgramaInserir.java
@@ -0,0 +1,52 @@
+package application;
+
+import db.DB;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class ProgramaInserir {
+    public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "INSERT INTO public.seller "
+                    + "(\"Name\", \"Email\", \"BirthDate\", \"BaseSalary\", \"DepartmentId\") "
+                    +"VALUES (?, ?, ?, ?, ?);",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            st.setString(1, "Carlos Silva");
+            st.setString(2, "carlos@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("25/05/1971").getTime()));
+            st.setDouble(4, 13000.0);
+            st.setInt(5, 4);
+
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id: " + id);
+                }
+            } else {
+                System.out.println("No rows affected!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        finally {
+            db.DB.closeStatement(st);
+            db.DB.closeConnection();
+        }
+    }
+}
--- src/application/ProgramaListar.java
+++ src/application/ProgramaListar.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Program {
+public class ProgramaListar {
     public static void main(String[] args) {
         Connection conn = null;
         Statement st = null;
