2c1ff6f370232b72d29215ffc97408a499d3c0e5
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-05-26 07:36:33 -0300

    Inserindo valores na tabela de Departamentos de forma literal

--- src/application/ProgramaInserir.java
+++ src/application/ProgramaInserir.java
@@ -12,9 +12,11 @@ public class ProgramaInserir {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
+        String sql = null;
 
         try {
             conn = DB.getConnection();
+            /*
             st = conn.prepareStatement(
                     "INSERT INTO public.seller "
                     + "(\"Name\", \"Email\", \"BirthDate\", \"BaseSalary\", \"DepartmentId\") "
@@ -26,10 +28,16 @@ public class ProgramaInserir {
             st.setDate(3, new java.sql.Date(sdf.parse("25/05/1971").getTime()));
             st.setDouble(4, 13000.0);
             st.setInt(5, 4);
+            */
+
+            sql = "INSERT INTO public.department (\"Name\") VALUES ('D1'),('D2');";
+            st = conn.prepareStatement(
+                    sql, Statement.RETURN_GENERATED_KEYS
+            );
 
             int rowsAffected = st.executeUpdate();
 
-            if(rowsAffected > 0) {
+            if (rowsAffected > 0) {
                 ResultSet rs = st.getGeneratedKeys();
                 while (rs.next()) {
                     int id = rs.getInt(1);
@@ -41,10 +49,7 @@ public class ProgramaInserir {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        finally {
+        } finally {
             db.DB.closeStatement(st);
             db.DB.closeConnection();
         }
